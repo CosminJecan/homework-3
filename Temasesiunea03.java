736b382d64831fbb7c346eda5aee84ee4d68f2f2
CosminJecan <85880477+CosminJecan@users.noreply.github.com>
2021-06-21 00:13:54 +0300

    Add files via upload
    Tema sesiunea 3:
    Greeting.

---
+++ Temasesiunea03.java
@@ -0,0 +1,17 @@
+package org.example;
+import java.util.Scanner;
+
+public class Temasesiunea03 {
+    public static void main(String[] args) {
+        String a;
+        System.out.println("\t" + "Introduceti numele dumneavoastra:");
+
+        a = new Scanner(System.in).nextLine();
+
+        if (a.length() >= 1) {
+            System.out.println("Hello, " + a + "!");
+            } else {
+                System.out.println("Hello, stranger!");
+            }
+        }
+    }
